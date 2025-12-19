4f3f14707d70a18f93e26721dc6fd4fe8119011a
Hafsah1-Coder <hafsahalam12@gmail.com>
2025-12-18 19:07:50 -0500

    Update clock.js

--- clock.js
+++ clock.js
@@ -1,23 +1,29 @@
 const WEEK = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
 
-function updateTime() {
-	var now = new Date();
+function zeroPadding(num, digit) {
+	return String(num).padStart(digit, '0');
+}
 
-	document.getElementById("time").innerText = 
+function updateTime() {
+	const now = new Date();
+
+	const timeEL = document.getElementById("time");
+	const dateEl = document.getElementById("date");
+
+	if (!timeEl || !dateEl) return;
+
+	timeEl.innerText = 
 		zeroPadding(now.getHours(), 2) + ":" +
 		zeroPadding(now.getMinutes(), 2) + ":" +
 		zeroPadding(now.getSeconds(), 2);
-
-	document.getElementById("date").innerText = 
+	dateEl.innerText = 
 		now.getFullYear() + "-" +
 		zeroPadding(now.getMonth() + 1, 2) + "-" +
 		zeroPadding(now.getDate(), 2) + " " +
 		WEEK[now.getDay()];
 }
 
-updateTime();
-setInterval(updateTime, 1000);
-
-function zeroPadding(num, digit) {
-	return String(num).padStart(digit, '0');
-}
+window.addEventListener("DOMContentLoaded", () => {
+	updateTime();
+	setInterval(updateTime, 1000);
+});
